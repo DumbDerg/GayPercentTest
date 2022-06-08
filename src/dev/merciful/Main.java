21c9169ccc14d5d7da6955ea7df79b353f9d822c
DumbDergMerciful <transamjustin@gmail.com>
2022-06-08 12:04:08 -0500

    Initial commit

---
+++ src/dev/merciful/Main.java
@@ -0,0 +1,13 @@
+package dev.merciful;
+
+import java.util.Random;
+import java.util.concurrent.Delayed;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+            int x = random.nextInt(0, 101);
+            System.out.println("You're "+x+"% gay! UwU \nHappy Pride Month!");
+    }
+}
