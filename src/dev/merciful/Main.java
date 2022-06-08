54a0f60224654acf7f68901e15c05c1db38d0b6b
DumbDergMerciful <transamjustin@gmail.com>
2022-06-08 13:49:44 -0500

    Updated code UwU

--- src/dev/merciful/Main.java
+++ src/dev/merciful/Main.java
@@ -1,13 +1,25 @@
 package dev.merciful;
 
 import java.util.Random;
-import java.util.concurrent.Delayed;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
+        boolean retest = true;
         Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        while (retest) {
             int x = random.nextInt(0, 101);
-            System.out.println("You're "+x+"% gay! UwU \nHappy Pride Month!");
+            System.out.println("Who do you want to test? (Type \"self\" if you want to test yourself!)");
+            String name = scanner.next();
+            if (name == "self") {
+                System.out.println("You're " + x + "% gay! UwU \nHappy Pride Month!");
+            }else {
+                System.out.println(name + " is " + x + "% gay! UwU\nHappy Pride Month!");
+            }
+            System.out.println("Do you want to do the test again? (true or false)");
+            retest = scanner.nextBoolean();
+        }
+
     }
 }
